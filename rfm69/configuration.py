c029e147b6192a0d061871648d539252ed904611
GvKosiberg <666kosyak666@gmail.com>
2018-01-23 10:49:57 +0300

    mb yes

--- rfm69/configuration.py
+++ rfm69/configuration.py
@@ -91,7 +91,9 @@ class PacketConfig1(RegisterValue):
         self.dc_free = self.DCFreeOff
         self.crc = True
         self.crc_auto_clear_off = False
-        self.address_filtering = 0b10
+        #123
+        self.address_filtering = 0b00
+        #self.address_filtering = 0b10
 
 
 class Temperature1(RegisterValue):
