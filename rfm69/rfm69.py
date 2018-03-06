c1d623866ed3e7600e37dc25f3c3ee936c1e727f
GvKosinberg <666kosyak666@gmail.com>
2018-03-06 13:15:34 +0300

    remove test logs

--- rfm69/rfm69.py
+++ rfm69/rfm69.py
@@ -101,12 +101,8 @@ class RFM69(object):
         start = time()
         self.packet_ready_event = Event()
         self.wrt_event = Event()
-        self.log.debug("Type b4 pris %s" % type(self.wrt_event))
-        self.log.debug("Type of inc e %s" % type(wrt_event))
         self.wrt_event = wrt_event
 
-        self.log.debug("Type of wrt e in rfm69 lib %s" % type(self.wrt_event))
-        self.log.debug("Val of wrt e in rfm69 lib %s" % self.wrt_event)
         self.rx_restarts = 0
         GPIO.add_event_detect(self.dio0_pin, GPIO.RISING, callback=self.payload_ready_interrupt)
         self.set_mode(OpMode.RX)
